adab19c1bdd5db5958c1ca24a0d796e840ab0875
Martin Vera <tinchovera8@hotmail.com>
2025-03-07 09:55:18 -0300

    back-button 2

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -1,16 +1,17 @@
-import { Card, Title, UnstyledButton } from "@mantine/core";
+import { Card, Text, Title, UnstyledButton } from "@mantine/core";
 import PropTypes from "prop-types";
 import classes from "./ScoreCard.module.css";
 
-export const ScoreCard = ({ title, set, point, onPointScored }) => {
+export const ScoreCard = ({ title, set, point, onPointScored, commandVoice }) => {
   return (
     <Card shadow="sm">
       <UnstyledButton
         className={classes.redButton}
         onClick={onPointScored} // Use the passed function
       >
-        <Title order={3} ta="center" mb={0}>
+        <Title order={3} ta="center" mb={0} style={{ lineHeight: 1 }}>
           {title}
+					<Text c="dimmed" mb={0}>({commandVoice})</Text>					
         </Title>
         <Title order={2} ta="center" mb={-25}>
           Set: {set}
@@ -28,4 +29,5 @@ ScoreCard.propTypes = {
   set: PropTypes.number.isRequired, // Should be number, not string
   point: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired, // Can be number or string (for "A")
   onPointScored: PropTypes.func.isRequired, // Ensure function is required
+	commandVoice: PropTypes.string.isRequired,
 };
